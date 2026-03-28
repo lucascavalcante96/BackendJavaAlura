682b9375eeaa0d2674c29bea404fe50b7a7c496b
Lucas Cavalcante <lulucas.cavalcante@gmail.com>
2026-03-27 23:46:01 -0300

    condicionais if else

---
+++ primeiroprojeto/src/Condicional.java
@@ -0,0 +1,20 @@
+public class Condicional {
+    static void main() {
+        int anoDeLancamento = 2022;
+        boolean incluidoNoPlano = true;
+        double notaDoFilme = 8.1;
+        String tipoPlano = "plus";
+
+        if (anoDeLancamento >= 2022){
+            System.out.println("Lancamentos que os clientes estão curtindo");
+        } else {
+            System.out.println("Filme retrô que vale a pena assistir!");
+        }
+
+        if (incluidoNoPlano == true || tipoPlano.equals("plus")) {
+            System.out.println("Filme liberado!");
+        } else  {
+            System.out.println("Filme nao liberado!");
+        }
+    }
+}
