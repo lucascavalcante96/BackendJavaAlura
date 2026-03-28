691880b6c095b7a4b51bf4277800b55be2967636
Lucas Cavalcante <lulucas.cavalcante@gmail.com>
2026-03-27 22:44:17 -0300

    aprendendo variaveis

--- HelloWord/src/Main.java
+++ HelloWord/src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     static void main() {
-        System.out.println("Hello World");
+        System.out.println("Esse é o Screen Math");
+        System.out.println("Filme: Top Gun: Maverick");
+
+        int anoDeLancamento = 2022;
+        System.out.println("Ano de lancamento: " + anoDeLancamento);
     }
 }
