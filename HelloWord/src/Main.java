668e00aec471216dfa0b0dceba3aa9d6e1372c05
Lucas Cavalcante <lulucas.cavalcante@gmail.com>
2026-03-27 22:47:42 -0300

    tipos no java

--- HelloWord/src/Main.java
+++ HelloWord/src/Main.java
@@ -5,5 +5,9 @@ public class Main {
 
         int anoDeLancamento = 2022;
         System.out.println("Ano de lancamento: " + anoDeLancamento);
+        boolean incluidoNoPlano =  true;
+        System.out.println("Incluido no plano: " + incluidoNoPlano);
+        double notaDoFilme = 8.1;
+        System.out.println("Nota do filme: " + notaDoFilme);
     }
 }
