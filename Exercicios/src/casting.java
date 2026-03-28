70cfb0ff5a212a2a892352fbb5cc4ed451cb360f
Lucas Cavalcante <lulucas.cavalcante@gmail.com>
2026-03-27 23:11:20 -0300

    exercicios java de fixacao

---
+++ Exercicios/src/casting.java
@@ -0,0 +1,9 @@
+public class casting {
+    static void main() {
+        double preco = 59.99;
+
+        int precoConvertido = (int) preco;
+
+        System.out.println(precoConvertido);
+    }
+}
---
+++ Exercicios/src/media.java
@@ -0,0 +1,9 @@
+public class media {
+    static void main() {
+        double nota1 = 8;
+        double nota2 = 5;
+
+        double media = (nota1 + nota2) / 2;
+        System.out.println("Media: " + media);
+    }
+}
