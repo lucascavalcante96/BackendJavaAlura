549a10ddb4f9527d3d77245ed76dd593d1de6ea2
Lucas Cavalcante <lulucas.cavalcante@gmail.com>
2026-03-27 22:58:02 -0300

    outros tipos no java

--- HelloWord/src/Main.java
+++ HelloWord/src/Main.java
@@ -5,9 +5,17 @@ public class Main {
 
         int anoDeLancamento = 2022;
         System.out.println("Ano de lancamento: " + anoDeLancamento);
+
         boolean incluidoNoPlano =  true;
         System.out.println("Incluido no plano: " + incluidoNoPlano);
+
         double notaDoFilme = 8.1;
         System.out.println("Nota do filme: " + notaDoFilme);
+
+        double media = (9.8 + 7.0 +  6.3) / 3;
+        System.out.println("Media: " + media);
+
+        String sinopse = "Filme de aventura com galã dos anos 80.";
+        System.out.println("Sinopse: " + sinopse);
     }
 }
