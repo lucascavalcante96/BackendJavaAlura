ab8cd4bed01bdba387f1f6fc7ee9ebf06b115981
Lucas Cavalcante <lulucas.cavalcante@gmail.com>
2026-03-30 20:04:31 -0300

    novos metodos para o filme

--- screenmatch/src/Filme.java
+++ screenmatch/src/Filme.java
@@ -1,12 +1,21 @@
 public class Filme {
     String nome;
     int duracaoEmMinutos;
-    double avaliacao;
+    double somaDasAvaliacoes;
     boolean incluidoNoPlano;
     int anoDeLancamento;
+    int totalDeAvaliacoes;
 
     void exibeFichaTecnica(){
         System.out.println("Nome do Filme: " + nome);
         System.out.println("Ano de Lancamento: " + anoDeLancamento);
+    }
+
+    void avalia(double nota){
+        somaDasAvaliacoes += nota;
+        totalDeAvaliacoes ++;
+    }
+    double pegaMedia(){
+        return somaDasAvaliacoes / totalDeAvaliacoes;
     }
 }
