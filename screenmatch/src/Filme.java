5847aca3fbcf5ca1fb428d4d127525c032dbed88
Lucas Cavalcante <lulucas.cavalcante@gmail.com>
2026-03-30 19:50:57 -0300

    refazendo o screenmatch e criando a classe filme

---
+++ screenmatch/src/Filme.java
@@ -0,0 +1,7 @@
+public class Filme {
+    String nome;
+    int duracaoEmMinutos;
+    double avaliacao;
+    boolean incluidoNoPlano;
+    int anoDeLancamento;
+}
---
+++ screenmatch/src/Principal.java
@@ -0,0 +1,9 @@
+public class Principal {
+    static void main() {
+        Filme meuFilme = new Filme();
+        meuFilme.nome = "O poderoso chefão";
+        meuFilme.duracaoEmMinutos = 180;
+        meuFilme.anoDeLancamento = 1970;
+
+    }
+}
