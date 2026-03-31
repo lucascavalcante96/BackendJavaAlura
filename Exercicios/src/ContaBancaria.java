78fd2a435f6661bf7b7a64e2815db2f83308c3bb
Lucas Cavalcante <lulucas.cavalcante@gmail.com>
2026-03-30 21:25:02 -0300

    exercicio para criar uma classe bancaria e utilizar os geters e seters

---
+++ Exercicios/src/ContaBancaria.java
@@ -0,0 +1,29 @@
+public class ContaBancaria {
+    private int numeroConta;
+    private double saldo;
+    private  String titular;
+
+    public int getNumeroConta() {
+        return numeroConta;
+    }
+
+    public void setNumeroConta(int numeroConta) {
+        this.numeroConta = numeroConta;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+
+    public String getTitular() {
+        return titular;
+    }
+
+    public void setTitular(String titular) {
+        this.titular = titular;
+    }
+}
