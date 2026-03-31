047dd690964c55477e667b056fba1b10e961bbc2
Lucas Cavalcante <lulucas.cavalcante@gmail.com>
2026-03-31 16:22:44 -0300

    sobrescrita de classes

--- screenmatch/src/Principal.java
+++ screenmatch/src/Principal.java
@@ -8,16 +8,21 @@ public class Principal {
         meuFilme.setDuracaoEmMinutos(180);
         meuFilme.setAnoDeLancamento(1970);
 
-        meuFilme.exibeFichaTecnica();
-        meuFilme.avalia(8);
-        meuFilme.avalia(5);
-        meuFilme.avalia(10);
-
-        System.out.println("Total de avaliações" + meuFilme.getTotalDeAvaliacoes());
-        System.out.println(meuFilme.somaDasAvaliacoes);
-        System.out.println(meuFilme.pegaMedia());
+//        meuFilme.exibeFichaTecnica();
+//        meuFilme.avalia(8);
+//        meuFilme.avalia(5);
+//        meuFilme.avalia(10);
+//
+//        System.out.println("Total de avaliações" + meuFilme.getTotalDeAvaliacoes());
+//        System.out.println(meuFilme.somaDasAvaliacoes);
+//        System.out.println(meuFilme.pegaMedia());
 
         Serie lost = new Serie();
         lost.setNome("Lost");
+        lost.setAnoDeLancamento(2000);
+        lost.setTemperoda(10);
+        lost.setEpsodiosPorTemporada(10);
+        lost.setMinutosPorEpsodio(50);
+        System.out.println("Duração da Serie " + lost.getDuracaoEmMinutos());
     }
 }
