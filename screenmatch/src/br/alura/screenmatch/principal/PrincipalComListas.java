b284f80f10cd6e8aeae49c0eee00933871ab51ba
Lucas Cavalcante <lulucas.cavalcante@gmail.com>
2026-04-14 09:53:49 -0300

    for each e aprendendo instanceof

---
+++ screenmatch/src/br/alura/screenmatch/principal/PrincipalComListas.java
@@ -0,0 +1,37 @@
+package br.alura.screenmatch.principal;
+
+import br.alura.screenmatch.modelos.Filme;
+import br.alura.screenmatch.modelos.Serie;
+import br.alura.screenmatch.modelos.Titulo;
+
+import java.util.ArrayList;
+
+public class PrincipalComListas {
+    static void main() {
+        Filme meuFilme = new Filme("O poderoso chefão", 1970);
+        meuFilme.avalia(9);
+
+        Filme outroFilme = new Filme("Avatar", 2023);
+        outroFilme.avalia(6);
+
+        var novoFilme = new Filme("Dogville", 2003);
+        novoFilme.avalia(10);
+
+        Serie lost = new Serie("Lost", 2000);
+
+        ArrayList<Titulo> lista = new ArrayList<>();
+        lista.add(novoFilme);
+        lista.add(meuFilme);
+        lista.add(outroFilme);
+        lista.add(lost);
+
+        for (Titulo item : lista) {
+            System.out.println(item.getNome());
+            if (item instanceof Filme filme){
+                System.out.println("Classificação " + filme.getClassificacao());
+            }
+        }
+
+//        lista.forEach(titulo -> System.out.println(titulo));
+    }
+}
