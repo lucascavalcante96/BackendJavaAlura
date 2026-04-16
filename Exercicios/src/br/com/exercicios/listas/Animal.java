41534a7fc69f9c1ec1ee455668b339a131045ba7
Lucas Cavalcante <lulucas.cavalcante@gmail.com>
2026-04-16 08:09:17 -0300

    criando a classe animal

---
+++ Exercicios/src/br/com/exercicios/listas/Animal.java
@@ -0,0 +1,58 @@
+package br.com.exercicios.listas;
+
+public class Animal {
+    String nome;
+    String raca;
+    String cor;
+    String pelo;
+    double peso;
+    String sexo;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getRaca() {
+        return raca;
+    }
+
+    public void setRaca(String raca) {
+        this.raca = raca;
+    }
+
+    public String getCor() {
+        return cor;
+    }
+
+    public void setCor(String cor) {
+        this.cor = cor;
+    }
+
+    public String getPelo() {
+        return pelo;
+    }
+
+    public void setPelo(String pelo) {
+        this.pelo = pelo;
+    }
+
+    public double getPeso() {
+        return peso;
+    }
+
+    public void setPeso(double peso) {
+        this.peso = peso;
+    }
+
+    public String getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+}
